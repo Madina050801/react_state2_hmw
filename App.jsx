3548b9fc30eea4c4bb1c69dfe6c63b6f5f0b1279
abduraufova_ <92726131+Madina050801@users.noreply.github.com>
2021-12-10 17:22:08 +0500

    editttt

--- App.jsx
+++ App.jsx
@@ -1,38 +1,57 @@
 import React from "react";
-import {data} from './data';
-import './index.css';
+import { data } from "./data";
+import "./index.css";
 
 class Toshkent extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state={
-      name: '',
-      status: '',
+    this.state = {
+      name: "",
+      id: "",
+      active: null,
+      status: "",
       data: {
-        status: 'OK',
+        status: "OK",
         dataList: data,
-      }
-    }
+      },
+    };
   }
-  render(){
+  render() {
     console.log(this.state.data.dataList);
     const onDelete = (id) => {
-      let res = this.state.data.dataList.filter((value)=> value.id !== id)
+      let res = this.state.data.dataList.filter((value) => value.id !== id);
       this.setState({
         data: {
           ...this.state.data,
-          dataList:res,
+          dataList: res,
         },
       });
     };
 
-    const onChange=(e) => {
+    const onEdit = (id) => {
+      this.setState({ active: id });
+    };
+
+    const onChange = (e) => {
       console.log(e);
       this.setState({
         [e.target.name]: e.target.value,
       });
-    }
-    const onSave=()=> {
+    };
+
+    const onChangeID = (e, id, key) => {
+      var data = this.state.data.dataList.map((value) =>
+        value.id === id ? { ...value, id: e.target.value } : value
+      );
+
+      this.setState({
+        data: {
+          ...this.state.data,
+          dataList: data,
+        },
+      });
+    };
+    const onSave = () => {
       const newData = {
         id: Date.now(),
         name: this.state.name,
@@ -42,71 +61,72 @@ class Toshkent extends React.Component {
         data: {
           ...this.state.data,
           dataList: [...this.state.data.dataList, newData],
-        }
-      })
-    }
-    return(
-      <div className='container'>
-        <div>
-          <input placeholder='name' name='name' onChange={onChange} type="text" />
-          <input placeholder='status' name='status' onChange={onChange} type="text" /> 
-          <button onClick={onSave}>save</button>
-        </div>
-        
-        <div className='table'>
-        <table className='table'>
-            <tr>
-              <th>ID</th>
-              <th>Name</th>
-              <th>edit</th>
-            </tr>
-        {this.state.data.dataList.map((value)=> (
-          
-            <tr>
-              <td>{value.id}</td>
-              <td>{value.name}</td>
-              <td>
-                <button onClick={() => onDelete(value.id)}>delete</button>
-              </td>
-            </tr>
-          
-          // <h1>
-          //   {value.id} {value.name}
-          // </h1>
-        ))
-        }
-        </table>
+        },
+        name: "",
+        status: "",
+      });
+    };
+    return (
+      <div>
+        <input
+          value={this.state.name}
+          placeholder="name"
+          name="name"
+          onChange={onChange}
+          type="text"
+        />
+        <input
+          value={this.state.status}
+          placeholder="status"
+          name="status"
+          onChange={onChange}
+          type="text"
+        />
+        <button onClick={onSave}>save</button>
+
+        <div className="container">
+          <table border="1" className="table">
+            <thead>
+              <tr className="fixed">
+                <th>ID</th>
+                <th>Name</th>
+                <th>Status</th>
+                <th>Actions</th>
+                <th>Actions</th>
+                <th>Actions</th>
+                <th>Actions</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.state.data.dataList.map(({ id, name, status }) => (
+                <tr key={id}>
+                  <td className="fixed">
+                    {this.state.active === id ? (
+                      <input
+                        value={id}
+                        onChange={(e) => onChangeID(e, id, "id")}
+                      />
+                    ) : (
+                      id
+                    )}
+                  </td>
+                  <td>{name}</td>
+                  <td>{status} </td>
+                  <td>
+                    <button onClick={() => onDelete(id)}>delete</button>
+                    <button onClick={() => onEdit(id)}>
+                      {this.state.active === id ? "save" : "edit"}
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
       </div>
-    )
-    }
-    // return(
-    //   <div>
-    //     <h1>Hi State {this.state.count}</h1>
-    //     <h1>Name: {this.state.name}</h1>
-    //     <h1>Surname: {this.state.surname}</h1>
-
-    //     <button name='count' onClick={(e) =>increment(e)}>+</button>
-    //     <button name='count' onClick={decrement}>-</button>
-    //     <br />
-
-    //     <input placeholder='name' name='name' onChange={onChange} type="text" />
-    //     <input placeholder='surname' name='surname' onChange={onChange} type="text" />
-    //     { !this.state.data.dataList.length ? (
-    //       <h1>no data</h1>
-    //     ) : (
-    //       this.state.data.dataList.map((value)=>(
-    //         <div key={value.id}>
-    //           <h1>
-    //           {value.id} {value.name} <button onClick={()=> onDelete(value.id) }>delete</button>
-    //           </h1>
-    //         </div>
-    //       )))
-      //   }
-      // </div>
-      // );  
-    // }
+    );
+  }
 }
-
 
 export default Toshkent;
